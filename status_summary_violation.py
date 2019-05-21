bd1745aa7897689013eb91bdd74639ae41fc1b29
Jake.Jung <39234175+jgsung2619@users.noreply.github.com>
2019-05-21 12:05:44 +0900

    Update status_summary_violation.py
    addlabels 추가

--- status_summary_violation.py
+++ status_summary_violation.py
@@ -11,28 +11,7 @@ ID = "goosung.jung"
 PASSWORD = "wjdrntjd3570!"
 # .txt file to description
 
-def timeConvertor(rawDate):
-    lastUpdateList = rawDate.split(sep="T")
-    lastUpdateString = lastUpdateList[0]
-    lastUpdate = datetime.strptime(lastUpdateString, "%Y-%m-%d")
-    print(lastUpdate)
-    timeDeltaCalculator(lastUpdate)
-
-def timeDeltaCalculator(lastUpdate):
-    now = datetime.now()
-    print(now)
-    delta = now - lastUpdate  # 7 days, 21:21:20.939894, datetime
-    dayOfDelta = delta.days  # 7, int
-    print(dayOfDelta)
-    findGroundRuleViolation(dayOfDelta)
-
-def findGroundRuleViolation(delta):
-    if delta > 7 :
-        print("addLabels")
-    elif delta <= 7 :
-        print("OK")
-
-def findStatusSummaryUpdate(issue_changelog):
+def findStatusSummaryUpdate(issue, issue_changelog):
     maxResults = issue_changelog.maxResults
     histories = issue_changelog.histories
     rawDateList = []
@@ -41,16 +20,47 @@ def findStatusSummaryUpdate(issue_changelog):
         for item in history.items:
             if item.field == "Status Summary":
                 rawDateList.append(history.created)
-    rawDate = rawDateList[-2]
+    rawDate = rawDateList[-1]
     print(rawDate)
-    timeConvertor(rawDate)
+    timeConvertor(issue,rawDate)
 
+def timeConvertor(issue, rawDate):
+    lastUpdateList = []
+    lastUpdateList = rawDate.split(sep="T")
+    lastUpdateString = lastUpdateList[0]
+    lastUpdate = datetime.strptime(lastUpdateString, "%Y-%m-%d")
+    print(lastUpdate)
+    timeDeltaCalculator(issue,lastUpdate)
+
+def timeDeltaCalculator(issue, lastUpdate):
+    now = datetime.now()
+    print(now)
+    delta = now - lastUpdate  # 7 days, 21:21:20.939894, datetime
+    dayOfDelta = delta.days  # 7, int
+    print(dayOfDelta)
+    findGroundRuleViolation(issue,dayOfDelta)
+
+def findGroundRuleViolation(issue, delta):
+    if delta > 6 :
+        addLabels(issue, delta)
+    elif delta <= 6 :
+        print("OK")
+
+def addLabels(issue, delta):
+    issue_labels = issue.fields.labels
+    if delta > 13:
+        issue_labels.append('Status_Summary2회미입력')
+    elif delta > 6:
+        issue_labels.append('Status_Summary1회미입력')
+
+    issue.update(fields={"labels": issue_labels})
+    print(str(issue_labels))
 
 if __name__ == "__main__" :
     # jira Handle open
     jira = JIRA(DevTracker, basic_auth=(ID, PASSWORD))
     #filter=49494 : 1_webOS_5.0_Epic_OverDue_Initiative_pm
-    issue_search_convert = jira.search_issues("key=TVPLAT-19005", maxResults=1000, expand="changelog")
+    issue_search_convert = jira.search_issues("filter=49607", maxResults=1000, expand="changelog")
     # issue_search_convert = jira.search_issues("filter=49520", maxResults=100)
     print("### status summary 업데이트 누락 이니셔티브 ###")
     # Create New Jira Tickets
@@ -61,7 +71,7 @@ if __name__ == "__main__" :
         issue_summary = issue.fields.summary
         issue_changelog = issue.changelog
         print('{} : {} ({})'.format(issue_key, issue_summary, issue_assignee))
-        findStatusSummaryUpdate(issue_changelog)
+        findStatusSummaryUpdate(issue, issue_changelog)
 
     # raw = "2019-05-12T10:11:11"
     # timeConvertor(raw)
