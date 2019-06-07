80e3023a1ff5ebb1c54078a04b37224353f79959
Jake.Jung <39234175+jgsung2619@users.noreply.github.com>
2019-06-07 14:07:25 +0900

    labeling add

--- status_summary_violation.py
+++ status_summary_violation.py
@@ -21,7 +21,7 @@ def findStatusSummaryUpdate(issue, issue_changelog):
             if item.field == "Status Summary":
                 rawDateList.append(history.created)
     rawDate = rawDateList[-1]
-    print(rawDate)
+    print("LOG: ",rawDate)
     timeConvertor(issue,rawDate)
 
 def timeConvertor(issue, rawDate):
@@ -29,39 +29,46 @@ def timeConvertor(issue, rawDate):
     lastUpdateList = rawDate.split(sep="T")
     lastUpdateString = lastUpdateList[0]
     lastUpdate = datetime.strptime(lastUpdateString, "%Y-%m-%d")
-    print(lastUpdate)
+    print("LOG: ",lastUpdate)
     timeDeltaCalculator(issue,lastUpdate)
 
 def timeDeltaCalculator(issue, lastUpdate):
     now = datetime.now()
-    print(now)
+    print("LOG: ", now)
     delta = now - lastUpdate  # 7 days, 21:21:20.939894, datetime
     dayOfDelta = delta.days  # 7, int
-    print(dayOfDelta)
+    print("LOG: ", dayOfDelta)
     findGroundRuleViolation(issue,dayOfDelta)
 
 def findGroundRuleViolation(issue, delta):
     if delta > 6 :
         addLabels(issue, delta)
     elif delta <= 6 :
-        print("OK")
+        print("LOG: 점검결과 OK!")
 
 def addLabels(issue, delta):
     issue_labels = issue.fields.labels
     if delta > 13:
-        issue_labels.append('Status_Summary2회미입력')
-    elif delta > 6:
+        try :
+            issue_labels.remove('Status_Summary1회미입력')
+            issue_labels.append('Status_Summary2회미입력')
+            print("LOG: 1회->2회")
+        except:
+            issue_labels.append('Status_Summary2회미입력')
+            print("LOG: 바로 2회 라벨링. 왜 1회 누락되었는지 점검해주세요!")
+    elif delta > 6 & delta <= 13:
         issue_labels.append('Status_Summary1회미입력')
+        print("LOG: 1회 라벨링")
 
     issue.update(fields={"labels": issue_labels})
-    print(str(issue_labels))
+    print("LOG: ", str(issue_labels))
 
 if __name__ == "__main__" :
     # jira Handle open
     jira = JIRA(DevTracker, basic_auth=(ID, PASSWORD))
     #filter=49494 : 1_webOS_5.0_Epic_OverDue_Initiative_pm
-    issue_search_convert = jira.search_issues("filter=49607", maxResults=1000, expand="changelog")
-    # issue_search_convert = jira.search_issues("filter=49520", maxResults=100)
+    # issue_search_convert = jira.search_issues("filter=49607", maxResults=1000, expand="changelog")
+    issue_search_convert = jira.search_issues("filter=49520", maxResults=1000, expand='changelog')
     print("### status summary 업데이트 누락 이니셔티브 ###")
     # Create New Jira Tickets
     for key in issue_search_convert:
