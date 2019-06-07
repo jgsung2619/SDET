4d4436c436a2e580aad980ff552c14be5cde5eeb
Jake.Jung <39234175+jgsung2619@users.noreply.github.com>
2019-06-07 16:31:26 +0900

    Update demo_comment_tool_v1.0.py

--- demo_comment_tool_v1.0.py
+++ demo_comment_tool_v1.0.py
@@ -11,16 +11,16 @@ DevTracker = 'http://hlm.lge.com/issue'
 QTracker = 'http://hlm.lge.com/qi'
 ID = input("AD계정 : ")
 PASSWORD = input("Passwd : ")
+filterNo = input("filter number : ")
 
 
 def planingComments(tc, assignee):
 
-    comments = "[~" + assignee + """] Owner님! 본 Initiative는 이번 2019_IR2SP19 Sprint Demo 대상 항목입니다.
+    comments = "[~" + assignee + """] Owner님. 본 Initiative는 이번 2019_IR2SP21 Sprint Demo 대상 항목입니다.
     하기와 같이 collab Page에 Demo 계획 수립 요청드립니다.
-    부디 일정 준수 부탁드립니다.
 
-    *Demo취합page:* http://collab.lge.com/main/pages/viewpage.action?pageId=955662722
-    *기한: ~5/16(목), 오전 10시*
+    *Demo취합page:* http://collab.lge.com/main/pages/viewpage.action?pageId=987499094
+    *기한: ~6/13(목), 오전 10시*
 
     Sprint Demo의 취지는 Milestone에 명시된 산출물과 DoD를 팀장님 주관하에 Review를 진행하는 것입니다.
     현물Demo가 가능한 건은 이번 Sprint기간 내 팀장님과 일정 조율하시어 Demo진행 부탁드립니다.
@@ -30,11 +30,11 @@ def planingComments(tc, assignee):
 
 def delayComments(tc, assignee):
 
-    comments = "[~" + assignee + """] Owner님! 본 Initiative는 이번 2019_IR2SP19 Sprint Demo 대상 항목입니다.
+    comments = "[~" + assignee + """] Owner님! 본 Initiative는 이번 2019_IR2SP21 Sprint Demo 대상 항목입니다.
     하기와 같이 collab Page에 Demo 계획 수립이 되지 않아 코멘트드립니다.
     되도록 오늘까지 계획 수립하시어 콜랩 업데이트 부탁드립니다.
 
-    *Demo취합page:* http://collab.lge.com/main/pages/viewpage.action?pageId=955662722
+    *Demo취합page:* http://collab.lge.com/main/pages/viewpage.action?pageId=987499094
     *기한: 금일 퇴근 전*
 
     Sprint Demo의 취지는 Milestone에 명시된 산출물과 DoD를 팀장님 주관하에 Review를 진행하는 것입니다.
@@ -46,8 +46,7 @@ def delayComments(tc, assignee):
 if __name__ == "__main__":
     # jira Handle open
     jira = JIRA(DevTracker, basic_auth=(ID, PASSWORD))
-    # issue_search_convert = jira.search_issues("filter=49413", maxResults=1000) # 최초 계획 수립
-    issue_search_convert = jira.search_issues("filter=49413 and (labels not in (demo:sp19, demo:skip:sp19, demo:delay:sp19) or labels is empty) ", maxResults=1000) # 계획 수립 지연
+    issue_search_convert = jira.search_issues("filter="+ filterNo +" and (labels not in (demo:sp21, demo:skip:sp21, demo:delay:sp21) or labels is empty)", maxResults=1000) # 계획 수립 지연
     print("### 관리대상 이니셔티브 ###")
     # Create New Jira Tickets
     for key in issue_search_convert:
@@ -56,5 +55,5 @@ if __name__ == "__main__":
         issue_summary = issue.fields.summary
         issue_assignee = issue.fields.assignee.name
         print('{} : {} ({})'.format(issue_key, issue_summary, issue_assignee))
-        # planingComments(issue, issue_assignee)
-        delayComments(issue, issue_assignee)
+        planingComments(issue, issue_assignee)
+        # delayComments(issue, issue_assignee)
